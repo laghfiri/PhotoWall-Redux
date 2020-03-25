fe743d2db7215b92bf1d57fc33b1ab9fc460a244
unknown <fatimazahra.laghfiri@gmail.com>
2020-03-25 20:47:04 +0100

    first commit

---
+++ src/Components/Main.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react'
+import PhotoWall from './PhotoWall'
+import AddPhoto from './AddPhoto'
+import {Route, Link} from 'react-router-dom'
+
+
+class Main extends Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return ( 
+        
+        <div>
+            <h1> 
+                <Link to="/"> Photowall </Link>    
+             </h1>
+            <Route exact path = "/" render={() => (
+                 <div>
+                      <PhotoWall {...this.props} />   
+                 </div>
+
+            )}/> 
+
+            <Route path= "/AddPhoto" render = {({history}) => (
+                <AddPhoto {...this.props} onHistory={history}/>
+            )}/>
+
+            
+         </div>
+        )
+    }
+
+}
+
+
+export default Main
---
+++ src/data/posts.js
@@ -0,0 +1,12 @@
+const posts = [{
+    id: "0",
+    description: "landscape",
+    imageLink: "https://www.planetware.com/photos-large/SEY/best-islands-seychelles.jpg"
+  }, {
+    id: "1",
+    description: "Mountain",
+    imageLink: "https://www.visitrussia.com/content/images/hotel_static/std/citygroup725x460/citygroup/293.jpg"
+  }]
+
+export default posts;
+
